ae65b7317fca8fba1c66ceeb732dcdcd6d97a960
masterkillersystem <juniorboliviacel@gmail.com>
2020-01-08 22:06:11 -0400

    modific

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+
+  /*
+    En ocasiones el usuario puede volver al login
+    aun si ya existe una sesion iniciada, lo correcto
+    es no mostrar otra ves el login sino redireccionarlo
+    a su pagina principal mientras exista una sesion entonces
+    creamos un archivo que controle el redireccionamiento
+  */
+  session_start();
+  // isset verifica si existe una variable o eso creo xd
+  if(isset($_SESSION['id'])){
+    header('location: controller/redirec.php');
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Login</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  
+  <!-- Custom styles for this template-->
+  <link href="assets/css/sb-admin-2.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="assets/css/sweetalert.css">
+
+</head>
+
+
+<body class="bg-gradient-primary">
+
+
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="card card-login mx-auto mt-5">
+
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              
+              <div class="col-lg-12">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-700 mb-4"><i class="fas fa-lg fa-graduation-cap"></i> Sistema de Gestion Academica</h1>
+                  </div>
+
+                    <div class="form-group">
+                        <i class="fas fa-user"></i> <label id="exampleInputEmail">Usuario:</label>
+                      <input type="email" id="user"  class="form-control form-control-user" required="required" autofocus="autofocus" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                    </div>
+
+                    <div class="form-group">
+                      <i class="fas fa-lock"></i> <label id="exampleInputPassword">Password:</label>
+                      <input type="password" id="clave" class="form-control form-control-user" required="required" placeholder="Password...">
+                    </div>
+
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Recuerdame</label>
+                      </div>
+                    </div>
+
+                    <button type="submit" name="button" id="login" class="btn btn-primary btn-lg btn-user btn-block">
+                     <i class="fas fa-lg fa-sign-in-alt fa-fw"></i> Iniciar Sesion
+                    </button>
+
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="recuperar_pass">Olvidaste tu Contraseña?</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="assets/js/sb-admin-2.min.js"></script>
+
+  <script src="assets/js/operaciones.js"></script>
+  
+  <script src="assets/js/sweetalert.min.js"></script>
+
+</body>
+
+</html>
+
---
+++ view/admin/index.php
@@ -0,0 +1,251 @@
+
+
+    <!-- ucfirst convierte la primera letra en mayusculas de una cadena -->
+    Hola administrador <?php echo ucfirst($_SESSION['nombre']); ?>
+    <a href="../../controller/cerrarSesion.php">
+      <button type="button" name="button">Cerrar sesion</button>
+    </a>
+
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+          </div>
+
+          <!-- Content Row -->
+          <div class="row">
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary bg-gradient-primary shadow h-100 py-2">
+                <div class="card-body-home">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-gray-200 text-uppercase mb-1">Total Docente</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-200">4000</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users fa-3x text-gray-400"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-success bg-gradient-success shadow h-100 py-2">
+                <div class="card-body-home">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-gray-200 text-uppercase mb-1">Total Estudiante</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-200">15000</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-user-graduate fa-3x text-gray-400"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info bg-gradient-info shadow h-100 py-2">
+                <div class="card-body-home">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-gray-200 text-uppercase mb-1">Personal</div>
+                      <div class="row no-gutters align-items-center">
+                        <div class="col-auto">
+                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-200">500</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-list-alt fa-3x text-gray-400"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-warning bg-gradient-warning shadow h-100 py-2">
+                <div class="card-body-home">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-gray-200 text-uppercase mb-1">Tutores</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-200">18000</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-restroom fa-3x text-gray-400"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Content Row -->
+
+          <div class="row">
+
+            <!-- Area Chart -->
+            <div class="col-xl-12 col-lg-7">
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Lista de Estudiantes</h6>
+                  <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                      <div class="dropdown-header">Dropdown Header:</div>
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+                </div>
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>System Architect</td>
+                      <td>Edinburgh</td>
+                      <td>61</td>
+                      <td>2011/04/25</td>
+                      <td>$320,800</td>
+                    </tr>
+                    <tr>
+                      <td>Garrett Winters</td>
+                      <td>Accountant</td>
+                      <td>Tokyo</td>
+                      <td>63</td>
+                      <td>2011/07/25</td>
+                      <td>$170,750</td>
+                    </tr>
+                    <tr>
+                      <td>Ashton Cox</td>
+                      <td>Junior Technical Author</td>
+                      <td>San Francisco</td>
+                      <td>66</td>
+                      <td>2009/01/12</td>
+                      <td>$86,000</td>
+                    </tr>
+                    <tr>
+                      <td>Cedric Kelly</td>
+                      <td>Senior Javascript Developer</td>
+                      <td>Edinburgh</td>
+                      <td>22</td>
+                      <td>2012/03/29</td>
+                      <td>$433,060</td>
+                    </tr>
+                    <tr>
+                      <td>Airi Satou</td>
+                      <td>Accountant</td>
+                      <td>Tokyo</td>
+                      <td>33</td>
+                      <td>2008/11/28</td>
+                      <td>$162,700</td>
+                    </tr>
+                    <tr>
+                      <td>Brielle Williamson</td>
+                      <td>Integration Specialist</td>
+                      <td>New York</td>
+                      <td>61</td>
+                      <td>2012/12/02</td>
+                      <td>$372,000</td>
+                    </tr>
+                    <tr>
+                      <td>Herrod Chandler</td>
+                      <td>Sales Assistant</td>
+                      <td>San Francisco</td>
+                      <td>59</td>
+                      <td>2012/08/06</td>
+                      <td>$137,500</td>
+                    </tr>
+                    <tr>
+                      <td>Rhona Davidson</td>
+                      <td>Integration Specialist</td>
+                      <td>Tokyo</td>
+                      <td>55</td>
+                      <td>2010/10/14</td>
+                      <td>$327,900</td>
+                    </tr>
+                    <tr>
+                      <td>Colleen Hurst</td>
+                      <td>Javascript Developer</td>
+                      <td>San Francisco</td>
+                      <td>39</td>
+                      <td>2009/09/15</td>
+                      <td>$205,500</td>
+                    </tr>
+                    <tr>
+                      <td>Sonya Frost</td>
+                      <td>Software Engineer</td>
+                      <td>Edinburgh</td>
+                      <td>23</td>
+                      <td>2008/12/13</td>
+                      <td>$103,600</td>
+                    </tr>
+                    <tr>
+                      <td>Jena Gaines</td>
+                      <td>Office Manager</td>
+                      <td>London</td>
+                      <td>30</td>
+                      <td>2008/12/19</td>
+                      <td>$90,560</td>
+                    </tr>
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          
+</div>
+</div>
+</div>
+</div>
+        <!-- /.container-fluid -->
+
+
+
+
+    
+  </body>
+</html>
